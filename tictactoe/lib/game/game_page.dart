255b1d61f07682112bc82d0df36ef0516b6bf0e7
Jairzinno Henriquez <jairzinnohenriquez@gmail.com>
2019-09-15 16:35:05 +0200

    extracted game page widgets

--- tictactoe/lib/game/game_page.dart
+++ tictactoe/lib/game/game_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/core/page.dart';
 import 'package:tictactoe/game/game_space.dart';
+import 'package:tictactoe/model/position.dart';
 import 'package:tictactoe/service/game_service.dart';
 import 'package:tictactoe/turn/turns_list.dart';
 import 'package:tictactoe/turn/turns_page.dart';
@@ -18,72 +19,160 @@ class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Page(
-      onMobileBuilder: (context, _, __) => Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              widget.gameService.stop();
-              Navigator.of(context).pop();
-            },
-          ),
-          title: Text('Game'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.list),
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return TurnsPage(gameService: widget.gameService);
-                }));
-              },
-            )
-          ],
+      onMobileBuilder: (context, _, __) => _MobileGamePage(
+        gameService: widget.gameService,
+        positionTapped: (position) {
+          widget.gameService.positionTapped(position);
+        },
+        onStop: () {
+          onStopGame(context);
+        },
+        onTurns: () {
+          onGoToTurns(context);
+        },
+      ),
+      onTabletBuilder: (context, _, __) => _TabletGamePage(
+        gameService: widget.gameService,
+        positionTapped: (position) {
+          widget.gameService.positionTapped(position);
+        },
+        onStop: () {
+          onStopGame(context);
+        },
+      ),
+      onWebBuilder: (context, _, __) => _WebGamePage(
+        gameService: widget.gameService,
+        positionTapped: (position) {
+          widget.gameService.positionTapped(position);
+        },
+      ),
+    );
+  }
+
+  void onStopGame(BuildContext context) {
+    widget.gameService.stop();
+    Navigator.of(context).pop();
+  }
+
+  void onGoToTurns(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return TurnsPage(gameService: widget.gameService);
+    }));
+  }
+}
+
+class _MobileGamePage extends StatelessWidget {
+  final GameService gameService;
+  final VoidCallback onStop;
+  final VoidCallback onTurns;
+  final ValueChanged<Position> positionTapped;
+
+  const _MobileGamePage(
+      {Key key,
+      this.gameService,
+      this.onStop,
+      this.onTurns,
+      this.positionTapped})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: onStop,
         ),
-        body: Container(
-          child: Center(
-            child: GameSpace(
-              gameService: widget.gameService,
-              onPositionTap: (position) {
-                widget.gameService.positionTapped(position);
-              },
-            ),
+        title: Text('Game'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: onTurns,
+          )
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: GameSpace(
+            gameService: gameService,
+            onPositionTap: positionTapped,
           ),
         ),
       ),
-      onTabletBuilder: (context, _, __) => Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              widget.gameService.stop();
-              Navigator.of(context).pop();
-            },
+    );
+  }
+}
+
+class _TabletGamePage extends StatelessWidget {
+  final GameService gameService;
+  final VoidCallback onStop;
+  final ValueChanged<Position> positionTapped;
+
+  const _TabletGamePage(
+      {Key key, this.gameService, this.onStop, this.positionTapped})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: onStop,
+        ),
+        title: Text('Game'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width / 2,
+            child: GameSpace(
+              gameService: gameService,
+              onPositionTap: positionTapped,
+            ),
           ),
-          title: Text('Game'),
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width / 2,
-              child: GameSpace(
-                gameService: widget.gameService,
-                onPositionTap: (position) {
-                  widget.gameService.positionTapped(position);
-                },
-              ),
+          Container(
+            width: MediaQuery.of(context).size.width / 2,
+            child: TurnsList(
+              turns: gameService.history,
             ),
-            Container(
-              width: MediaQuery.of(context).size.width / 2,
-              child: TurnsList(
-                turns: widget.gameService.history,
-              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _WebGamePage extends StatelessWidget {
+  final GameService gameService;
+  final ValueChanged<Position> positionTapped;
+
+  const _WebGamePage({Key key, this.gameService, this.positionTapped})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width / 2,
+            child: GameSpace(
+              gameService: gameService,
+              onPositionTap: positionTapped,
             ),
-          ],
-        ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 2,
+            child: TurnsList(
+              turns: gameService.history,
+            ),
+          ),
+        ],
       ),
     );
   }
